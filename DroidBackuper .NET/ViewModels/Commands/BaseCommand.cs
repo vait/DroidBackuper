86b5626cef03bf142245ab386dff090bcbbbb2b1
Victor V. Grekov <victor.grek@gmail.com>
2022-02-14 23:35:49 +0300

    magrate

--- DroidBackuper .NET/ViewModels/Commands/BaseCommand.cs
+++ DroidBackuper .NET/ViewModels/Commands/BaseCommand.cs
@@ -7,136 +7,135 @@ using System.Windows.Media;
 
 namespace DroidBackuper.NET.ViewModels.Commands
 {
-    public abstract class BaseCommand<T> : MarkupExtension, ICommand where T: class, ICommand, new()
-    {
-        #region MarkupExtension
-        /// <summary>
-        /// A singleton instance.
-        /// </summary>
-        private static T command;
+	public abstract class BaseCommand<T> : MarkupExtension, ICommand where T : class, ICommand, new()
+	{
+		#region MarkupExtension
+		/// <summary>
+		/// A singleton instance.
+		/// </summary>
+		private static T command;
 
-        /// <summary>
-        /// Gets a shared command instance.
-        /// </summary>
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            if (command == null) command = new T();
-            return command;
-        }
+		/// <summary>
+		/// Gets a shared command instance.
+		/// </summary>
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (command == null) command = new T();
+			return command;
+		}
 
-        #endregion
+		#endregion
 
-        #region Constructors
+		#region Constructors
 
-        /// <summary>
-        /// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
-        /// </summary>
-        public BaseCommand()
-        {
-        }
-        #endregion
+		/// <summary>
+		/// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
+		/// </summary>
+		public BaseCommand()
+		{
+		}
+		#endregion
 
-        #region ICommand Members
+		#region ICommand Members
 
-        ///<summary>
-        ///Defines the method that determines whether the command can execute in its current state.
-        ///</summary>
-        ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
-        ///<returns>
-        ///true if this command can be executed; otherwise, false.
-        ///</returns>
-        public abstract bool CanExecute(object parameter);
+		///<summary>
+		///Defines the method that determines whether the command can execute in its current state.
+		///</summary>
+		///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+		///<returns>
+		///true if this command can be executed; otherwise, false.
+		///</returns>
+		public abstract bool CanExecute(object parameter);
 
-        ///<summary>
-        ///Occurs when changes occur that affect whether or not the command should execute.
-        ///</summary>
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
+		///<summary>
+		///Occurs when changes occur that affect whether or not the command should execute.
+		///</summary>
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
 
-        ///<summary>
-        ///Defines the method to be called when the command is invoked.
-        ///</summary>
-        ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
-        public abstract void Execute(object parameter);
+		///<summary>
+		///Defines the method to be called when the command is invoked.
+		///</summary>
+		///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
+		public abstract void Execute(object parameter);
 
-        #endregion
+		#endregion
 
-        /// <summary>
-        /// Resolves the window that owns the TaskbarIcon class.
-        /// </summary>
-        /// <param name="commandParameter"></param>
-        /// <returns></returns>
-        protected Window GetTaskbarWindow(object commandParameter)
-        {
-            //get the showcase window off the taskbaricon
-            var tb = commandParameter as TaskbarIcon;
-            return tb == null ? null : TryFindParent<Window>(tb);
-        }
+		/// <summary>
+		/// Resolves the window that owns the TaskbarIcon class.
+		/// </summary>
+		/// <param name="commandParameter"></param>
+		/// <returns></returns>
+		protected Window GetTaskbarWindow(object commandParameter)
+		{
+			//get the showcase window off the taskbaricon
+			var tb = commandParameter as TaskbarIcon;
+			return tb == null ? null : TryFindParent<Window>(tb);
+		}
 
-        #region TryFindParent helper
+		#region TryFindParent helper
 
-        /// <summary>
-        /// Finds a parent of a given item on the visual tree.
-        /// </summary>
-        /// <typeparam name="T">The type of the queried item.</typeparam>
-        /// <param name="child">A direct or indirect child of the
-        /// queried item.</param>
-        /// <returns>The first parent item that matches the submitted
-        /// type parameter. If not matching item can be found, a null
-        /// reference is being returned.</returns>
-        public static T TryFindParent<T>(DependencyObject child)
-            where T : DependencyObject
-        {
-            //get parent item
-            DependencyObject parentObject = GetParentObject(child);
+		/// <summary>
+		/// Finds a parent of a given item on the visual tree.
+		/// </summary>
+		/// <typeparam name="T">The type of the queried item.</typeparam>
+		/// <param name="child">A direct or indirect child of the
+		/// queried item.</param>
+		/// <returns>The first parent item that matches the submitted
+		/// type parameter. If not matching item can be found, a null
+		/// reference is being returned.</returns>
+		public static T TryFindParent<T>(DependencyObject child)
+			where T : DependencyObject
+		{
+			//get parent item
+			DependencyObject parentObject = GetParentObject(child);
 
-            //we've reached the end of the tree
-            if (parentObject == null) return null;
+			//we've reached the end of the tree
+			if (parentObject == null) return null;
 
-            //check if the parent matches the type we're looking for
-            T parent = parentObject as T;
-            if (parent != null)
-            {
-                return parent;
-            }
-            else
-            {
-                //use recursion to proceed with next level
-                return TryFindParent<T>(parentObject);
-            }
-        }
+			//check if the parent matches the type we're looking for
+			T parent = parentObject as T;
+			if (parent != null)
+			{
+				return parent;
+			}
+			else
+			{
+				//use recursion to proceed with next level
+				return TryFindParent<T>(parentObject);
+			}
+		}
 
-        /// <summary>
-        /// This method is an alternative to WPF's
-        /// <see cref="VisualTreeHelper.GetParent"/> method, which also
-        /// supports content elements. Keep in mind that for content element,
-        /// this method falls back to the logical tree of the element!
-        /// </summary>
-        /// <param name="child">The item to be processed.</param>
-        /// <returns>The submitted item's parent, if available. Otherwise
-        /// null.</returns>
-        public static DependencyObject GetParentObject(DependencyObject child)
-        {
-            if (child == null) return null;
-            ContentElement contentElement = child as ContentElement;
+		/// <summary>
+		/// This method is an alternative to WPF's
+		/// <see cref="VisualTreeHelper.GetParent"/> method, which also
+		/// supports content elements. Keep in mind that for content element,
+		/// this method falls back to the logical tree of the element!
+		/// </summary>
+		/// <param name="child">The item to be processed.</param>
+		/// <returns>The submitted item's parent, if available. Otherwise
+		/// null.</returns>
+		public static DependencyObject GetParentObject(DependencyObject child)
+		{
+			if (child == null) return null;
+			ContentElement contentElement = child as ContentElement;
 
-            if (contentElement != null)
-            {
-                DependencyObject parent = ContentOperations.GetParent(contentElement);
-                if (parent != null) return parent;
+			if (contentElement != null)
+			{
+				DependencyObject parent = ContentOperations.GetParent(contentElement);
+				if (parent != null) return parent;
 
-                FrameworkContentElement fce = contentElement as FrameworkContentElement;
-                return fce != null ? fce.Parent : null;
-            }
+				FrameworkContentElement fce = contentElement as FrameworkContentElement;
+				return fce != null ? fce.Parent : null;
+			}
 
-            //if it's not a ContentElement, rely on VisualTreeHelper
-            return VisualTreeHelper.GetParent(child);
-        }
+			//if it's not a ContentElement, rely on VisualTreeHelper
+			return VisualTreeHelper.GetParent(child);
+		}
 
-        #endregion
-
-    }
+		#endregion
+	}
 }
